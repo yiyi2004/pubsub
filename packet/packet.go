51e064fa31474027ba08ff6b4d7a17f1a204ac5b
demon <huohuojunzc@gmail.com>
2019-09-15 23:22:26 +0800

    update
    add packet

--- packet/packet.go
+++ packet/packet.go
@@ -1,8 +1,21 @@
 package packet
 
+import "github.com/nats.go"
+
 // Packet represents a packet can contain any type of message
 // Packet should have a type field to record the message type
 // and another type to record the packet type such as: ...
 // emmm...
 type Packet interface {
+	PkgName() string
+	Type() string
+	Payload() []byte
+}
+
+// NatsPacket -
+type NatsPacket struct {
+	Name string
+	Type string
+
+	Msg *nats.Msg
 }
